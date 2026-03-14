83b3edc4966a6a12751833d3c9cb9ebf0af390cf
Sahil Ahmed <sahilahmed0029@gmail.com>
2026-03-14 18:54:40 +0530

    update backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,29 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// export const getProducts = async () => {
+//   const res = await API.get("/products");
+//   return res.data;
+// };
+
+// export const getProductBySlug = async (slug) => {
+//   const res = await API.get(`/products/${slug}`);
+//   return res.data;
+// };
+
+// export const getEMIPlans = async (slug) => {
+//   const res = await API.get(`/products/${slug}/emiplans`);
+//   return res.data;
+// };
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const getProducts = async () => {
